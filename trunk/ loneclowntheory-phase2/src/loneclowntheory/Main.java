ad3bc3b9646571e8dc0eb62e954b94a651df5ded
brandon.t.andersen@gmail.com <brandon.t.andersen@gmail.com@8ace4819-84db-4760-bbf2-9b4d753b83fb>
2011-03-24 00:34:52 +0000

    Working on test cases in Main.java.
    I think everything else works. DAC is in effect using our old ACM, grant and checkRights, plus a simplified version of revoke.

--- trunk/ loneclowntheory-phase2/src/loneclowntheory/Main.java
+++ trunk/ loneclowntheory-phase2/src/loneclowntheory/Main.java
@@ -25,125 +25,112 @@ public class Main
         LCTBellLaPadula467 lct;
         LCTIntegrityManager467 lctInteg;
 
+        // *********************************************************************
+        // BLP TEST CASES
+        // *********************************************************************
+
         System.out.println("*** TESTING BLP ***");
 
         lct = new LCTBellLaPadula467();
 
         Category467[] c1 =
         {
-            Category467.AZ, Category467.CA
+            Category467.AZ, Category467.CA, Category467.NM, Category467.TX
         };
-        SecurityLevel467 sl = new SecurityLevel467(SensitivityLevel467.SECRET, c1);
-        lct.newSubject("s1", sl);
 
         Category467[] c2 =
         {
             Category467.AZ
         };
-        sl = new SecurityLevel467(SensitivityLevel467.SECRET, c2);
-        lct.newSubject("s2", sl);
 
         Category467[] c3 =
         {
-            Category467.ABQ
+            Category467.PHX
         };
-        sl = new SecurityLevel467(SensitivityLevel467.CONFIDENTIAL, c3);
-        lct.newSubject("s3", sl);
 
         Category467[] c4 =
         {
+            Category467.PHX, Category467.SFO
         };
-        sl = new SecurityLevel467(SensitivityLevel467.CONFIDENTIAL, c4);
-        lct.newObject("o1", sl);
-
-        System.out.println("1 " + lct.dominates("subject0", "s2"));
-
-        System.out.println("2.0 " + lct.dominates("s1", "s2"));
-        System.out.println("2.1 " + lct.dominates("s2", "s1"));
-
-        System.out.println("3.0 " + lct.updateSL("s1", new SecurityLevel467(SensitivityLevel467.SECRET, c2)));
-        System.out.println("3.1 " + lct.dominates("s2", "s1"));
-
-        System.out.println("4.0 " + lct.dominates("s3", "o1"));
-
-        System.out.println("4.1 " + lct.classifyOL("o1", new SecurityLevel467(SensitivityLevel467.SECRET, c2)));
-
-        System.out.println("4.2 " + lct.dominates("s3", "o1"));
-
-        System.out.println("5 " + lct.declassifyOL("s3", "o1", sl));
-
-        System.out.println("6 " + lct.declassifyOL("subject0", "o1", sl));
-
-        System.out.println("7.0 " + lct.access("subject0", "o1", "r"));
-        System.out.println("7.1 " + lct.access("subject0", "o1", "w"));
-        System.out.println("7.2 " + lct.access("subject0", "o1", "a"));
-        System.out.println("7.3 " + lct.access("subject0", "o1", "e"));
-
-        System.out.println("8.0 " + lct.access("s1", "o1", "r"));
-        System.out.println("8.1 " + lct.access("s1", "o1", "w"));
-        System.out.println("8.2 " + lct.access("s1", "o1", "a"));
-        System.out.println("8.3 " + lct.access("s1", "o1", "e"));
-
-        System.out.println("9.0 " + lct.access("s2", "o1", "r"));
-        System.out.println("9.1 " + lct.access("s2", "o1", "w"));
-        System.out.println("9.2 " + lct.access("s2", "o1", "a"));
-        System.out.println("9.3 " + lct.access("s2", "o1", "e"));
-
-        System.out.println("10.0 " + lct.access("s3", "o1", "r"));
-        System.out.println("10.1 " + lct.access("s3", "o1", "w"));
-        System.out.println("10.2 " + lct.access("s3", "o1", "a"));
-        System.out.println("10.3 " + lct.access("s3", "o1", "e"));
 
         Category467[] c5 =
         {
-            Category467.AZ, Category467.TX
         };
-        sl = new SecurityLevel467(SensitivityLevel467.SECRET, c5);
-        System.out.println("11.0 " + lct.updateSL("s1", sl));
 
-        System.out.println("*** TESTING BIBA ***");
+        lct.newSubject("s1", new SecurityLevel467(SensitivityLevel467.SECRET, c2));
+        lct.newObject("o1", new SecurityLevel467(SensitivityLevel467.SECRET, c2));
 
-        lctInteg = new LCTIntegrityManager467();
+        System.out.println("1.0 " + lct.access("s1", "o1", "r")); // NO - missing discretionary right
+        System.out.println("1.1 " + lct.grant("subject0", "s1", "r", "o1")); // OK
+        System.out.println("1.2 " + lct.grant("subject0", "s1", "w", "o1")); // OK
+        System.out.println("1.3 " + lct.grant("subject0", "s1", "a", "o1")); // OK
+        System.out.println("1.4 " + lct.grant("subject0", "s1", "e", "o1")); // OK
+        System.out.println("1.5 " + lct.access("s1", "o1", "r")); // OK - has discretionary right and same level
+        System.out.println("1.6 " + lct.access("s1", "o1", "w")); // OK - has discretionary right and same level
+        System.out.println("1.7 " + lct.access("s1", "o1", "a")); // OK - has discretionary right and same level
+        System.out.println("1.8 " + lct.access("s1", "o1", "e")); // OK - has discretionary right and same level
 
-        sl = new SecurityLevel467(SensitivityLevel467.SECRET, c1);
-        lctInteg.newSubject("s1", sl, IntLevel467.SOME_EVIDENCE);
+        System.out.println("2.0 " + lct.updateSL("s1", new SecurityLevel467(SensitivityLevel467.CONFIDENTIAL, c2))); // OK
+        System.out.println("2.1 " + lct.access("s1", "o1", "r")); // NO - s1 no longer dominates o1 based on sensitivity
+        System.out.println("2.2 " + lct.access("s1", "o1", "w")); // NO - o1 dominates s1 based on sensitivity
+        System.out.println("2.3 " + lct.access("s1", "o1", "a")); // OK - o1 dominates s1 based on sensitivity
+        System.out.println("2.4 " + lct.access("s1", "o1", "e")); // OK - execute so okay
+
+        System.out.println("3.0 " + lct.updateSL("s1", new SecurityLevel467(SensitivityLevel467.SECRET, c2))); // OK
+        System.out.println("3.1 " + lct.access("s1", "o1", "r")); // OK
+
+        System.out.println("4.0 " + lct.updateSL("s1", new SecurityLevel467(SensitivityLevel467.TOP_SECRET, c1))); // NO - trying to set above max level
+
+        System.out.println("5.0 " + lct.updateSL("s1", new SecurityLevel467(SensitivityLevel467.SECRET, c2))); // OK
+        System.out.println("5.1 " + lct.access("s1", "o1", "r")); // NO - s1 no longer dominates o1 based on categories
 
 
-        sl = new SecurityLevel467(SensitivityLevel467.SECRET, c2);
-        lctInteg.newSubject("s2", sl, IntLevel467.UNVERIFIED);
-
-
-        sl = new SecurityLevel467(SensitivityLevel467.CONFIDENTIAL, c3);
-        lctInteg.newSubject("s3", sl, IntLevel467.FALSE);
-
-
-        sl = new SecurityLevel467(SensitivityLevel467.CONFIDENTIAL, c4);
-        lctInteg.newObject("o1", sl, IntLevel467.VERIFIED);
-
-        System.out.println("1 " + lctInteg.dominates("subject0", "s2"));
-
-        System.out.println("2.0 " + lctInteg.dominates("s1", "s2"));
-        System.out.println("2.1 " + lctInteg.dominates("s2", "s1"));
-
-        System.out.println("3.1 " + lctInteg.dominates("s2", "s1"));
-
-        System.out.println("4.0 " + lctInteg.dominates("s3", "o1"));
-        System.out.println("4.2 " + lctInteg.dominates("s3", "o1"));
-
-        System.out.println("7.0 " + lctInteg.access("subject0", "o1", "r"));
-        System.out.println("7.1 " + lctInteg.access("subject0", "o1", "w"));
-        System.out.println("7.3 " + lctInteg.access("subject0", "o1", "e"));
-
-        System.out.println("8.0 " + lctInteg.access("s1", "o1", "r"));
-        System.out.println("8.1 " + lctInteg.access("s1", "o1", "w"));
-        System.out.println("8.3 " + lctInteg.access("s1", "o1", "e"));
-
-        System.out.println("9.0 " + lctInteg.access("s2", "o1", "r"));
-        System.out.println("9.1 " + lctInteg.access("s2", "o1", "w"));
-        System.out.println("9.3 " + lctInteg.access("s2", "o1", "e"));
-
-        System.out.println("10.0 " + lctInteg.access("s3", "o1", "r"));
-        System.out.println("10.1 " + lctInteg.access("s3", "o1", "w"));
-        System.out.println("10.3 " + lctInteg.access("s3", "o1", "e"));
+//        // *********************************************************************
+//        // BIBA TEST CASES
+//        // *********************************************************************
+//        System.out.println("*** TESTING BIBA ***");
+//
+//        lctInteg = new LCTIntegrityManager467();
+//
+//        sl = new SecurityLevel467(SensitivityLevel467.SECRET, c1);
+//        lctInteg.newSubject("s1", sl, IntLevel467.SOME_EVIDENCE);
+//
+//
+//        sl = new SecurityLevel467(SensitivityLevel467.SECRET, c2);
+//        lctInteg.newSubject("s2", sl, IntLevel467.UNVERIFIED);
+//
+//
+//        sl = new SecurityLevel467(SensitivityLevel467.CONFIDENTIAL, c3);
+//        lctInteg.newSubject("s3", sl, IntLevel467.FALSE);
+//
+//
+//        sl = new SecurityLevel467(SensitivityLevel467.CONFIDENTIAL, c4);
+//        lctInteg.newObject("o1", sl, IntLevel467.VERIFIED);
+//
+//        System.out.println("1 " + lctInteg.dominates("subject0", "s2"));
+//
+//        System.out.println("2.0 " + lctInteg.dominates("s1", "s2"));
+//        System.out.println("2.1 " + lctInteg.dominates("s2", "s1"));
+//
+//        System.out.println("3.1 " + lctInteg.dominates("s2", "s1"));
+//
+//        System.out.println("4.0 " + lctInteg.dominates("s3", "o1"));
+//        System.out.println("4.2 " + lctInteg.dominates("s3", "o1"));
+//
+//        System.out.println("7.0 " + lctInteg.access("subject0", "o1", "r"));
+//        System.out.println("7.1 " + lctInteg.access("subject0", "o1", "w"));
+//        System.out.println("7.3 " + lctInteg.access("subject0", "o1", "e"));
+//
+//        System.out.println("8.0 " + lctInteg.access("s1", "o1", "r"));
+//        System.out.println("8.1 " + lctInteg.access("s1", "o1", "w"));
+//        System.out.println("8.3 " + lctInteg.access("s1", "o1", "e"));
+//
+//        System.out.println("9.0 " + lctInteg.access("s2", "o1", "r"));
+//        System.out.println("9.1 " + lctInteg.access("s2", "o1", "w"));
+//        System.out.println("9.3 " + lctInteg.access("s2", "o1", "e"));
+//
+//        System.out.println("10.0 " + lctInteg.access("s3", "o1", "r"));
+//        System.out.println("10.1 " + lctInteg.access("s3", "o1", "w"));
+//        System.out.println("10.3 " + lctInteg.access("s3", "o1", "e"));
     }
 }
